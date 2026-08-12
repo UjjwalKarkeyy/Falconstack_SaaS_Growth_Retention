ea6bb0b614032a49c396d654f8be72227b588cba
UjjwalKarkeyy <ujjwalkarki.official@gmail.com>
2026-08-12 14:16:09 +0545

    Done: 1. Cohort retention done. Meaning, retention metric completed 2. Revenue Concentration metrics completed
    ToDo:
    1. Revenue Metrics

--- sql/Scripts/retention_metrics.sql
+++ sql/Scripts/retention_metrics.sql
@@ -1,5 +1,5 @@
 /*
-CRR, Customer Churn Rate, Early-Stage Churn Rate, Cohort Retention
+    CRR, Customer Churn Rate, Early-Stage Churn Rate, Cohort Retention
 */
 
 WITH accounts_table AS (
@@ -144,18 +144,80 @@ SELECT
     ) AS early_churn_rate
 FROM
     new_early_churners AS n
+),
+-- create 30 and 90 days monthly signups churn
+cohort_data AS (
+SELECT
+    STRFTIME(m.mnth, '%Y-%m') AS mnth,
+    COUNT(DISTINCT CASE
+        WHEN a.signup_date >= m.mnth
+        AND a.signup_date < m.mnth + INTERVAL '1 month'
+        THEN a.account_id
+    END
+    ) AS monthly_signups,
+    COUNT(DISTINCT CASE
+        WHEN (a.signup_date >= m.mnth
+        AND a.signup_date < m.mnth + INTERVAL '1 month')
+        AND a.churn_date <= a.signup_date + INTERVAL '30 days'
+        THEN a.account_id
+    END
+    ) AS day_30,
+    COUNT(DISTINCT CASE
+        WHEN (a.signup_date >= m.mnth
+        AND a.signup_date < m.mnth + INTERVAL '1 month')
+        AND a.churn_date <= a.signup_date + INTERVAL '90 days'
+        THEN a.account_id
+    END
+    ) AS day_90
+FROM
+    accounts_table AS a
+CROSS JOIN mnths_group AS m
+GROUP BY
+    m.mnth
+ORDER BY
+    m.mnth
+),
+-- create cohort retention table
+cohort_retention_tbl AS (
+SELECT
+    c.mnth,
+    c.monthly_signups,
+    c.day_30,
+    ROUND(
+        100.0 * (c.monthly_signups - c.day_30)
+        / NULLIF(c.monthly_signups, 0)
+        , 2
+    ) AS retention_30_pct,
+    c.day_90,
+    ROUND(
+        100.0 * (c.monthly_signups- c.day_90)
+        / NULLIF(c.monthly_signups, 0)
+        , 2
+    ) AS retention_90_pct
+FROM
+    cohort_data AS c
 )
 
 SELECT
-    *
+    c.*
 FROM
-    early_churner_rate_tbl AS e;
+    cohort_retention_tbl AS c;
 
 /*
     TABLES OF VALUE:
         - Customer Retention Rate: crr_tbl
         - Customer Churn Rate: cus_churn_rate_tbl
         - Early-Stage Churn Rate: early_churner_rate_tbl 
+        - Cohort Retention: cohort_retention_tbl
         
     INSIGHTS:
+        - Customer retention is decreasing with Dec of 2024 taking the massive hit of 63.18% from prev (Nov, 2024) 83.52%
+        - Customer churn rate agrees with CRR as it also saw a massive increase from 16.48% in Nov, 2024 to 36.82% in Dec, 2024
+        - Early-Stage churn presents the problem where customers are leaving within 30 days of signup and that rate is highest in
+            Dec, 2024 with 55%
+        - The retention on 90 days of signup seem to decrease from the starting second-quarter of 2024 reaching as low as 23.08% in Sep, 2024
+            with retention rate not being able to cross 60%. Furthermore, the 30 days (early) retention decreases steadily and reaches low as
+            58.33% in Nov, 2024. The 90 days retention seem to be always lower than 30 days, but the differences between them is widened during
+            the end of 2024
+        - All this majorly hints looking into the 2024 year especially into 30 and 90 days period!
 */
--- sql/Scripts/revenue_concentration.sql
+++ sql/Scripts/revenue_concentration.sql
@@ -2,3 +2,77 @@
 Top-Customers Revenue Share, 
 Revenue Concentration Risk
 */
+
+WITH subscription_tbl AS (
+SELECT
+    s.* EXCLUDE (s.churn_flag),
+    CASE
+        WHEN s.churn_flag = 'TRUE' THEN 'T'
+        WHEN s.churn_flag = 'FALSE' THEN 'F'
+        ELSE 'U'
+    END AS churn_flag
+FROM
+    'D:\GitHub\Falconstack_SaaS_Growth_Retention\data\cleaned\subscriptions_cleaned.csv' AS s
+),
+-- create total arr for each account
+total_arr_per_acc AS (
+SELECT
+    s.account_id,
+    COALESCE(
+        SUM(CASE
+            WHEN s.churn_flag = 'F' THEN s.arr_amount
+            END
+            ), 0
+    ) AS total_arr
+FROM
+    subscription_tbl AS s
+GROUP BY
+    s.account_id
+),
+-- find total arr
+all_total_arr AS(
+SELECT
+    SUM(t.total_arr) AS total
+FROM
+    total_arr_per_acc AS t
+),
+-- create top 5 account's revenue share
+top_5_revenue_share AS (
+SELECT
+    t.account_id,
+    t.total_arr,
+    ROUND(
+        (t.total_arr / NULLIF(a.total, 0))
+        * 100.0
+        , 2
+    ) AS revenue_share
+FROM
+    total_arr_per_acc AS t
+CROSS JOIN all_total_arr AS a
+ORDER BY
+    t.total_arr DESC
+LIMIT 5
+),
+-- create total concentration share
+total_conc_share AS (
+SELECT
+    SUM(t.revenue_share) AS total_conc
+FROM
+    top_5_revenue_share AS t
+)
+
+SELECT
+    *
+FROM
+    total_conc_share;
+
+/*
+    TABLES OF VALUE:
+        - Top Customers Revenue Share: top_5_revenue_share
+        - Revenue Concentration Risk: total_conc_share
+    INSIGHTS:
+        - The top 5 customers account for only 5.07% of total active ARR,
+          indicating low revenue concentration and limited dependency
+          on a small number of customers.
+*/
+
